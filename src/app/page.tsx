aa962d9f6296539614f2e67448418f7c9a3ac4ee
Henry-Paul <164559529+Henry-Paul@users.noreply.github.com>
2025-12-12 09:45:40 +0530

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import ServicesGrid from "@/components/ServicesGrid";
+import Testimonials from "@/components/Testimonials";
+import SEOTool from "@/components/SEOTool";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <ServicesGrid />
+      <Testimonials />
+      <SEOTool />
+    </>
+  );
+}
